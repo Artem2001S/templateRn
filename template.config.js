bf0b108b21bab03974edd266d23d3e6b8397123f
Artem Kazaryan <artemkazaryan@Artems-MacBook-Air.local>
2022-09-13 13:46:22 +0300

    a

--- template.config.js
+++ template.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Placeholder used to rename and replace in files
   // package.json, index.json, android/, ios/
-  placeholderName: "Boilerplate",
+  placeholderName: "RNWebviewBoilerplate",
 
   // Directory with template
   templateDir: "./template",
